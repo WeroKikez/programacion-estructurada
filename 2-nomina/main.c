f7da4ab6d4f3ffa754b9149c021ad8f7140610ef
Emiliano Enriquez <enriquez.emiliano15@gmail.com>
2025-08-19 10:45:49 -0600

    2-Nomina: optimizando y funcionalidad para todos los puestos y horas añadidas

--- 2-nomina/main.c
+++ 2-nomina/main.c
@@ -10,7 +10,7 @@ const float porc_presta = .10;
 
 int main(int argc, char *argv[]) {
 	float paga, prestaciones, deducciones, sueldo_hora;
-	int puesto, horas_laboradas;
+	int puesto, horas_laboradas, horas_normales, horas_extras = 0, horas_triples = 0;
 
     printf("\nIntroduce las horas laboradas: ");
     scanf("%d", &horas_laboradas);
@@ -23,7 +23,9 @@ int main(int argc, char *argv[]) {
 	printf("\nSelecciona tu puesto: ");
 	scanf("%d", &puesto);
 	
-	switch(puesto) {
+	system("clear");
+    
+    switch(puesto) {
 		case 1:
             printf("\nPuesto: Profesor\n");
             sueldo_hora = sueldo_prof;
@@ -40,47 +42,66 @@ int main(int argc, char *argv[]) {
 			printf("\n-------------------------------\n");
 			printf("Error: Seleccion fuera de rango");
 	}
-
-    system("clear");
     
+    // Trabajo horas normales
     if(horas_laboradas <= 40) {
-        paga += horas_laboradas * sueldo_hora;
+        horas_normales = horas_laboradas;
         
-        printf("Sueldo por hora: $%.2f \n", sueldo_hora);
-        printf("Horas laboradas (normales): %d horas \n", horas_laboradas);
+    } else if( horas_laboradas > 40) { // trabajo horas extra
+        horas_normales = 40;
+        horas_extras = horas_laboradas - 40;
 
-        printf("Sueldo Base: $%.2f \n", paga);
-        
-        prestaciones = paga * porc_presta;
-        paga += prestaciones;
-        printf("Prestaciones (10%c): $%.2f \n", 37, prestaciones);
-
-        printf("Sueldo Antes de Deducciones: $%.2f \n", paga);
-        printf("Deducciones (");
-
-        
-        if(paga < 1000) {
-            deducciones = paga * .8;
-            printf("8%c): $%.2f \n", 37, deducciones);
+        // Trabajo más de 8 horas extra
+        if(horas_laboradas > 48) {
+            horas_extras = 8;
+            horas_triples = horas_laboradas - 48;
         }
-        else if (paga > 1000 && paga < 1500) {
-            deducciones = paga * .12;		
-            printf("12%c): $%.2f \n", 37, deducciones);
-        }
-        else {
-            deducciones = paga * .15;	
-            printf("15%c): $%.2f \n", 37, deducciones);
-        }
-
-        paga -= deducciones;
-        
-        printf("Sueldo final: $%.2f \n", paga);
-        
-    } else if( horas_laboradas <= 48) {
-        
-    } else {
-        
     }
+
+    printf("Sueldo por hora: $%.2f \n", sueldo_hora);
+
+    // Calculo de horas normales
+    paga += horas_normales * sueldo_hora;     
+    printf("Horas laboradas (normales): %d horas \n", horas_normales);
+
+    // Calculo de horas extras
+    paga += horas_extras * (sueldo_hora * 2);
+    printf("Horas laboradas (extras): %d horas \n", horas_extras);
+
+    // Calculo de horas extras triples
+    paga += horas_triples * (sueldo_hora * 3);
+    printf("Horas laboradas (dobles extras): %d horas \n", horas_triples);
+
+    // Mostrando sueldo antes de prestaciones y deducciones
+    printf("Sueldo Base: $%.2f \n", paga);
+    
+    // Calculo de prestaciones 
+    prestaciones = paga * porc_presta;
+    paga += prestaciones;
+    printf("Prestaciones (10%c): $%.2f \n", 37, prestaciones);
+
+    // Calculo de deducciones segun el ingreso después de prestaciones
+    printf("Sueldo Antes de Deducciones: $%.2f \n", paga);
+    printf("Deducciones (");
+
+    
+    if(paga < 1000) {
+        deducciones = paga * .8;
+        printf("8%c): $%.2f \n", 37, deducciones);
+    }
+    else if (paga > 1000 && paga < 1500) {
+        deducciones = paga * .12;		
+        printf("12%c): $%.2f \n", 37, deducciones);
+    }
+    else {
+        deducciones = paga * .15;	
+        printf("15%c): $%.2f \n", 37, deducciones);
+    }
+
+    paga -= deducciones;
+    
+    // Mostrando el sueldo final
+    printf("Sueldo final: $%.2f \n", paga);
 
     printf("\n");
 	
